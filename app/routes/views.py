b7e445b448501a9bf593989d94dd047859596ab1
blendirrustemi <blendirrustemi9@gmail.com>
2025-02-16 00:20:13 +0100

    finalised

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,9 +1,9 @@
 # app/routes/views.py
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
 from app.models.character import Character
 from app.utils.vikings_netflix_scraper import scrape_vikings_characters
 from app.utils.database import store_characters
-
+from app import db
 views_bp = Blueprint('views', __name__)
 
 @views_bp.route('/')
@@ -46,3 +46,21 @@ def scrape_vikings():
     store_characters()
 
     return "Scraping complete and data saved to database."
+
+
+@views_bp.route('/character/<int:id>', methods=['POST'])
+def update_character(id):
+    character = Character.query.get(id)
+    if character:
+        character.name = request.form['name']
+        character.actor = request.form['actor']
+        character.description = request.form['description']
+        character.photo_url = request.form['photo_url']
+        character.character_url = request.form['character_url']
+        
+        db.session.commit()  # Save changes to the database
+        
+        # Redirect to the updated character page using the correct endpoint
+        return redirect(url_for('views.character_page', id=character.id))  # Make sure this is 'views.character_page'
+    
+    return "Character not found", 404
